35cca7f8812e35d30c92f6a2c48986ae490264d5
radeonaru <radeonaru@gmail.com>
2025-01-20 15:59:47 +0700

    feat: remove base URL from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: 'https://lithiaproject.my.id/',
   plugins: [react()],
   resolve: {
     alias: {
