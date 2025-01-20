f54d8b5f76588b89950c36a2e6fc4f9a9f6ef434
Stella <radeonaru@gmail.com>
2025-01-20 15:26:19 +0700

    feat: update dependencies and enhance header navigation

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: 'https://lithiaproject.my.id/',
   plugins: [react()],
   resolve: {
     alias: {
