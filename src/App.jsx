ef92eb8b865b5f0f41f5e1b7b4bb93f200ef8ea3
radeonaru <radeonaru@gmail.com>
2025-01-20 15:47:56 +0700

    feat: update import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Portfolio from "@/pages/Portfolio";
+import Contact from "@/pages/Contact";
 
 const App = () => {
   return (
