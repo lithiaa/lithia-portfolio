14206f624d63e50366c4d4836b5717604b0552a0
Stella <radeonaru@gmail.com>
2025-04-20 22:38:13 +0700

    feat: enhance layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const Home = () => {
   return (
-    <section className="bg-gray-100 min-h-screen flex items-center justify-center p-6">
-      <div className="max-w-4xl text-left">
+    <section className="bg-gray-100 min-h-screen flex flex-col md:flex-row items-center justify-center p-6">
+      <div className="order-2 md:order-1 max-w-4xl text-center md:text-left py-6 md:py-0">
         <h1 className="text-4xl md:text-6xl font-bold text-gray-800">
-          Hi, I’m Muhammad  Bagus <span className="text-gray-800 underline">Indra</span>wan
+          Hi, I’m Muhammad Bagus <span className="text-gray-800 underline">Indra</span>wan
         </h1>
         <p className="mt-4 text-lg text-gray-600">
           A passionate <span className="text-gray-600 font-bold">Web Developer</span> who
           loves creating modern and responsive websites.
         </p>
       </div>
-      <div className="ring-offset-2 ring-2 ring-gray-800">
+      <div className="order-1 md:order-2 ring-offset-2 ring-2 ring-gray-800 mt-6 md:mt-0">
         <img
           src="/tenor.gif"
           alt="Lithia Project"
-          className="w-96 h-64 mt-6 md:mt-0"
+          className="w-full max-w-xs h-auto"
         />
       </div>
     </section>
